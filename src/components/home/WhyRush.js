0945c44d5e5d736d3a876a09a8d40308398036bd
Hillary Fan <fanhillary@Hillarys-MacBook-Pro.local>
2017-08-12 14:22:59 -0700

    rush banner with random pic

--- src/components/home/WhyRush.js
+++ src/components/home/WhyRush.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
 import {PageHeader} from 'react-bootstrap';
 import {Element} from 'react-scroll';
+import {Image} from 'react-bootstrap';
+import {View} from 'react-bootstrap';
 
 class WhyRush extends Component {
   render() {
     return (
       <Element name="why-rush" className="element">
-        <div className="why-rush big-cont">
-          <PageHeader className="title">This is the Why Rush Module</PageHeader>
+        <div className="rush-container">
+          <Image className="rush-banner" src={require('../../../public/images/rose.jpg')} responsive/>
+
+          <div className="overlay-text">
+            <h1 className="rush-title"> <strong> Rush Theta Tau </strong> </h1>
+
+            <span className="rush-subtitle"> Learn about the fraternity and meet the active body </span>
+
+
+            <div>
+              <span className="rush-promo"> JOIN US FOR FALL 2017 RUSH</span>
+              <a className="no-padding" href="/">
+                <button className="rush-button" type = "button"> Learn More </button>
+              </a>
+            </div>
+          </div>
         </div>
       </Element>
     );
@@ -15,4 +31,3 @@ class WhyRush extends Component {
 }
 
 export {WhyRush};
-
