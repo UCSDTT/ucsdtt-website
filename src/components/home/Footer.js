340c3125fa0ea6b928d8a172af0288e873bc23ed
Brian Chan <alto1020@gmail.com>
2017-08-13 16:08:39 -0700

    Added footer

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -1,11 +1,34 @@
 import React, {Component} from 'react';
-import {PageHeader} from 'react-bootstrap';
+import {Grid, Col, Row} from 'react-bootstrap';
+import '../../../node_modules/font-awesome/css/font-awesome.min.css';
 
 class Footer extends Component {
   render() {
     return (
-        <div className="footer big-cont">
-            <PageHeader className="title">This is the Footer Module</PageHeader>
+        <div className="footer dark-bg">
+            <Grid className="footer-padding">
+              <Row className="show-grid">
+                <Col xs={6} md={4} className="widget"><div className="widget-header">Want to learn more?</div><br/>asdf</Col>
+                <Col xs={6} md={4} className="widget"><div className="widget-header">Visit our neighboring chapters</div><br/>asdf</Col>
+                <Col xsHidden md={4} className="widget"><div className="widget-header">Credits</div><br/>asdf</Col>
+              </Row>
+            </Grid>
+            <div className="media-icons">
+              <Grid>
+                <Row className="show-grid">
+                  <Col sm={2} mdPush={4}>
+                    <a href="https://www.facebook.com/thetatau.ucsd/">
+                      <i className="fa fa-facebook fa-3x"></i>
+                    </a>
+                  </Col>
+                  <Col sm={2} mdPush={4}>
+                    <a href="https://instagram.com/thetatau_ucsd/">
+                      <i className="fa fa-instagram fa-3x"></i>
+                    </a>
+                  </Col>
+                </Row>
+              </Grid>
+            </div>
         </div>
     );
   }
