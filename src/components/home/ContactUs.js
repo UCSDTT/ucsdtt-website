165ca367f118e28aa0e171950c11e4803ba988ce
Brian Chan <alto1020@gmail.com>
2017-08-14 14:33:32 -0700

    Added layout for contact module

--- src/components/home/ContactUs.js
+++ src/components/home/ContactUs.js
@@ -1,13 +1,60 @@
 import React, {Component} from 'react';
-import {PageHeader} from 'react-bootstrap';
+import {FormGroup, FormControl, Button, InputGroup, Glyphicon, Grid, Col, Row} from 'react-bootstrap';
 import {Element} from 'react-scroll';
 
 class ContactUs extends Component {
   render() {
     return (
       <Element name="contact-us" className="element">
-        <div className="contact-us big-cont">
-          <PageHeader className="title">This is the Contact Us Module</PageHeader>
+        <div className="contact-us">
+        <h1>Contact Us</h1>
+          <Grid className="form-padding">
+            <Row>
+              <Col md={8} mdPush={2}>
+                <form>
+                  <FormGroup 
+                    controlId="formBasicName"
+                  >
+                    <InputGroup>
+                      <InputGroup.Addon>
+                        <Glyphicon glyph="user" />
+                      </InputGroup.Addon>
+                      <FormControl
+                        type="text"
+                        placeholder="Name*"
+                      />
+                      <FormControl.Feedback />
+                    </InputGroup>
+                  </FormGroup>
+
+                  <FormGroup controlId="formBasicEmail">
+                    <InputGroup>
+                      <InputGroup.Addon>
+                        <Glyphicon glyph="envelope" />
+                      </InputGroup.Addon>
+                      <FormControl
+                        type="text"
+                        placeholder="Email*"
+                      />
+                      <FormControl.Feedback />
+                    </InputGroup>
+                  </FormGroup>
+
+                  <FormGroup controlId="formControlsTextarea">
+                    <InputGroup>
+                      <InputGroup.Addon>
+                        <Glyphicon glyph="pencil" />
+                      </InputGroup.Addon>
+                      <FormControl componentClass="textarea" placeholder="Message*" />
+                    </InputGroup>
+                  </FormGroup>
+                  <Button className="contact-button" type="submit">
+                    Submit
+                  </Button>
+                </form>
+              </Col>
+            </Row>
+          </Grid>
         </div>
       </Element>
     );
