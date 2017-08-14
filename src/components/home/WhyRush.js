784976c815472e2b868b662131c2dcac4cdc5b4f
Brian Chan <alto1020@gmail.com>
2017-08-14 10:50:30 -0700

    Fixed vh issue for rush container

--- src/components/home/WhyRush.js
+++ src/components/home/WhyRush.js
@@ -5,24 +5,19 @@ import {Image} from 'react-bootstrap';
 class WhyRush extends Component {
   render() {
     return (
-      <div className="why-rush">
+      <div className="rush-container">
         <Element name="why-rush" className="element">
-          <div className="rush-container">
             <Image className="rush-banner" src={require('../../../public/images/rose.jpg')} responsive/>
-
             <div className="overlay-text">
               <h1 className="rush-title"> <strong> Rush Theta Tau </strong> </h1>
-
               <span className="rush-subtitle"> Learn about the fraternity and meet the active body </span>
               <div>
                 <span className="rush-promo"> JOIN US FOR FALL 2017 RUSH</span>
-
                 <a href="/rush">
                   <button className="rush-button" type="button"> Learn More </button>
                 </a>
               </div>
             </div>
-          </div>
         </Element>
       </div>
     );
