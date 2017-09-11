f8e919e3ef476c27170527c5691fdbe5f5a442ed
Hillary Fan <fanhillary@Hillarys-MBP.home>
2017-09-10 23:20:37 -0700

    icons added, bottom steps main structure

--- src/components/home/WhyRush/WhyRush.js
+++ src/components/home/WhyRush/WhyRush.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 import {Element} from 'react-scroll';
 import {Image, Grid, Row, Col} from 'react-bootstrap';
+import {MdWork, MdBuild, MdTextsms, MdInfo, MdLocationOn} from 'react-icons/lib/md';
 
 class WhyRush extends Component {
   render() {
@@ -10,39 +11,39 @@ class WhyRush extends Component {
           <div className="rush-container">
             <Grid className="rush-container">
               <Row className="rush-container">
-                <Col xs={12} md={4}>
+                <Col xs={12} md={6} lg={4}>
                   <Image className="" src={require('./images/rushflyer.jpg')} responsive/>
                 </Col>
-                <Col xs={12} md={8}>
+                <Col xs={12} md={6} lg={8}>
                   <Row>
                     <div className="rush-header">
                       <span className="rush-title"> Rush Fall 2017</span>
-                      <span className="rush-details">  Rush is a week full of events to learn about the fraternity and to meet the active body. </span>
+                      <span className="rush-subheader">  Rush is a week full of events to interact with the active body </span>
                     </div>
                   </Row>
                   <Row>
                     <Col xs={12} md={6}>
-                      <div className="rush-subheader">
-                        <span className="rush-title"> Info Night </span>
-                        <span className="rush-details"> Triton Statue, Tuesday 4/4  7:30 PM</span>
-                        <p className="rush-summary"> Come out and learn about our fraternity and meet the brothers of Theta Tau! </p>
+                      <div className="rush-subcontent">
+                        <span className="rush-title"> <MdInfo />  Info Night </span>
+                        <span className="rush-details"> <MdLocationOn /> Triton Statue, Tuesday 4/4  7:30 PM</span>
+                        <p className="rush-summary"> Come out to the first rush event to get a gist of what the fraternity is all about and meet the brothers of Theta Tau! Let's break the ice! </p>
                       </div>
-                      <div className="rush-subheader">
-                        <span className="rush-title"> Professional Night</span>
-                        <span className="rush-details"> Triton Statue, Thursday 4/6 7:30 PM</span>
-                        <p className="rush-summary"> Get your resumes critiqued and learn quality professional skills from actives in industry. Professional attire recommended. </p>
+                      <div className="rush-subcontent">
+                        <span className="rush-title"> <MdWork />  Professional Night</span>
+                        <span className="rush-details"> <MdLocationOn /> Triton Statue, Thursday 4/6 7:30 PM</span>
+                        <p className="rush-summary"> Get your resumes critiqued and learn quality professional skills from actives in industry. Professional attire is highly recommended. </p>
                       </div>
                     </Col>
                     <Col xs={12} md={6}>
-                      <div className="rush-subheader">
-                        <span className="rush-title"> Meet N Greet</span>
-                        <span className="rush-details"> Triton Statue, Wednesday 4/5 7:30 PM </span>
-                        <span className="rush-summary"> Engineer seeking engineer? Meet the brothers of Theta Tau speed-dating style. </span>
+                      <div className="rush-subcontent">
+                        <span className="rush-title"> <MdTextsms />  Meet N Greet</span>
+                        <span className="rush-details"> <MdLocationOn /> Triton Statue, Wednesday 4/5 7:30 PM </span>
+                        <span className="rush-summary"> Engineer seeking engineer? <br /> Well now is your chance to mingle with the brothers of Theta Tau -- speed-dating style. </span>
                       </div>
-                      <div className="rush-subheader">
-                        <span className="rush-title"> Engineer Night</span>
-                        <span className="rush-details"> RIMAC, Friday 4/7 6:30 PM </span>
-                        <span className="rush-summary"> Interact with other rushees and put your skills to the test with engineering-related games with the brothers! </span>
+                      <div className="rush-subcontent">
+                        <span className="rush-title"> <MdBuild />  Engineer Night</span>
+                        <span className="rush-details"> <MdLocationOn /> RIMAC, Friday 4/7 6:30 PM </span>
+                        <span className="rush-summary"> Interact with other rushees and put your skills to the test with engineering-related challenges! Be prepared to get hands on with the Brothers! </span>
                       </div>
                     </Col>
                   </Row>
@@ -51,50 +52,30 @@ class WhyRush extends Component {
               <Row>
                 <Col xs={12} md={3}>
                   <div className="rush-steps">
-                    <span className="step-title"> RUSH THETA TAU </span>
+                    <span className="step-title"> Rush </span>
                     <span className="step-details"> Learn about what the fraternity is all about. Get to meet the members of Theta Tau are made of.</span>
                   </div>
                 </Col>
                 <Col xs={12} md={3}>
                   <div className="rush-steps">
-                    <span className="step-title"> RUSH THETA TAU </span>
+                    <span className="step-title"> Interview </span>
                     <span className="step-details"> Learn about what the fraternity is all about. Get to meet the members of Theta Tau are made of.</span>
                   </div>
                 </Col>
                 <Col xs={12} md={3}>
                   <div className="rush-steps">
-                    <span className="step-title"> RUSH THETA TAU </span>
+                    <span className="step-title"> Bid </span>
                     <span className="step-details"> Learn about what the fraternity is all about. Get to meet the members of Theta Tau are made of.</span>
                   </div>
                 </Col>
                 <Col xs={12} md={3}>
                   <div className="rush-steps">
-                    <span className="step-title"> RUSH THETA TAU </span>
+                    <span className="step-title"> Pledge </span>
                     <span className="step-details"> Learn about what the fraternity is all about. Get to meet the members of Theta Tau are made of.</span>
                   </div>
                 </Col>
               </Row>
             </Grid>
-            //
-            // <Grid className="rush-container">
-            //   <Row className="rush-container">
-            //     <Col xs={12} md={8} className="image-div padding-0">
-            //       <Image className="rush-image" src={require('./images/rose.jpg')} responsive/>
-            //     </Col>
-            //     <Col xs={8} md={4} className="rush-blurb padding-0">
-            //         <div className="rush-text">
-            //           <span className="rush-bold"> Rush Theta Tau </span>
-            //           <span className="rush-details">  Learn about the fraternity and meet the active body </span>
-            //         </div>
-            //         <div>
-            //           <span className="rush-promo"> JOIN US FOR FALL 2017 RUSH</span>
-            //           <a href="/rush">
-            //             <button className="rush-button" type="button"> Learn More </button>
-            //           </a>
-            //         </div>
-            //     </Col>
-            //   </Row>
-            // </Grid>
           </div>
         </Element>
     );
