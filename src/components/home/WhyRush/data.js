c916812341edb741752ea8708c1245e3da5a00fa
Kaiser <kaiserkim167@gmail.com>
2017-09-23 04:14:22 -0700

    Changed background color for rush steps

---
+++ src/components/home/WhyRush/data.js
@@ -0,0 +1,24 @@
+const stepData = [
+  {
+    position: 'odd',
+    title: 'Rush',
+    description: 'Join us for a whole week of rush events! Learn about the fraternity,  interact with other rushees, and mingle with the members of Theta Tau.',
+  },
+  {
+    position: 'even',
+    title: 'Interview',
+    description: 'The Brothers of Theta Tau would like to find out more about you and hear about the reasons for your interest in Theta Tau through a formal interview.',
+  },
+  {
+    position: 'odd',
+    title: 'Bid',
+    description: 'If the Brothers of Theta Tau decide on you, an invitation will be sent to you. It is your choice to continue to the pledging process or decline the bid.',
+  },
+  {
+    position: 'even',
+    title: 'Pledge',
+    description: 'Once you accept your bid, you will then begin pledging. Prepare to spend a large amount of time and effort. "Approach it as a four unit upper division engineering course.',
+  },
+]
+
+export default stepData;
