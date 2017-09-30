2a87987284977150eda5d7d63496c9b567fb2bab
Kaiser <kaiserkim167@gmail.com>
2017-09-30 01:16:00 -0700

    Added real questions for FAQs and data for messenger actives and removed blinks for rush and members

--- src/components/home/FAQ/data.js
+++ src/components/home/FAQ/data.js
@@ -1,31 +1,39 @@
 const faqCol1 = [
   {
-    title: 'How do I be an active?',
-    body: 'A hackathon is an event, typically lasting multiple days, in which a large number of people meet to collaboratively build out their ideas using technology.',
+    title: 'What is a professional fraternity?',
+    body: 'A professional fraternity is a brotherhood consisting chiefly of individuals in a specific field of education, promoting professional development in addition to strengthening brotherly ties.'
   },
   {
-    title: 'Who can attend?',
-    body: 'Any college or high school students over 18 years of age from anywhere in the world are eligible. This includes graduate students, but be sure to understand any agreements you’ve made with your university with regards to research and IP. If you are enrolled at a UC campus but not yet 18, you are also eligible to participate.',
+    title: 'What is Rush?',
+    body: 'Rush is an opportunity for you to learn more about Theta Tau by meeting both our active and graduated brothers. Rush consists of multiple events that will give you a taste of what our fraternity stands for, whether you are a good fit for us, and whether we are a good fit for you. At the end of rush, we extend a limited number of interviews and bids. Those who receive bids can then decide whether or not they would like to pledge. Rushing is completely free of charge and there are no obligations.',
   },
   {
-    title: 'When should I show up and when should I leave?',
-    body: 'You should plan to arrive at the hackathon late Friday (Oct 20th) evening and to leave early Sunday (Oct 22nd) afternoon. A detailed schedule will be released closer to the event date.',
+    title: 'Who can rush?',
+    body: 'Students must be full-time students and be in good academic standing with the university (> 2.5 GPA).',
+  },
+  {
+    title: 'Why rush?',
+    body: 'Rush is an opportunity for individuals to see if a fraternity/sorority is the right fit for them. Conversely, fraternities and sororities search for motivated individuals to recruit during Rush.',
   },
 ]
 
 const faqCol2 = [
   {
-    title: 'What should I bring?',
-    body: 'Bring your student ID, computer, any external peripherals you may need (chargers, monitors, etc.), any other hardware that you definitely plan on using, and an open mind. Also bring a change of clothes, a toothbrush, and any other toiletries you may need. You may also bring a sleeping bag if you plan to nap.',
+    title: 'Why Theta Tau?',
+    body: 'Theta Tau is a diverse group of engineers who grow close together while striving towards similar goals. You will find our interests are wide and vary; some enjoy biking on a nice afternoon, others enjoy studying! However, all of us are committed to helping one another become the best possible engineer and person he/she can be.',
   },
   {
-    title: 'What if I dont have a team?',
-    body: 'We will host a team formation event at the beginning of the hackathon so that no one has to hack alone (unless they want to). We allow teams of up to four hackers. If you do have a team, we will do our best to accept you all. Make sure ALL members apply and include all other members’ addresses in their applications.',
+    title: 'What is a bid?',
+    body: 'A bid is a formal invitation to begin pledging, the process of becoming a brother.',
   },
   {
-    title: 'Do I have to pay to get in?',
-    body: 'Nope!',
+    title: 'How do you choose who receives a bid?',
+    body: 'Theta Tau searches for engineers who have a strong foundation in its three pillars: Brotherhood, Professionalism, and Service. We review each applicant as a whole (grades, personality, professionalism, resume, etc.) to determine whether we are the right fit for you.',
   },
+  {
+    title: 'What is pledging?',
+    body: 'Pledging is the process by which individuals integrate themselves into the brotherhood. It is led by pledge instructors, who assist the pledges in developing their professional skills, aiding the community, and most important of all, getting to know their future brothers.'
+  }
 ]
 
 export {faqCol1, faqCol2};
