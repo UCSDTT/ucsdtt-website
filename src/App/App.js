b7bcadd2944e6f172c5ea81c92ef2b69636b9466
Kaiser <kaiserkim167@gmail.com>
2017-10-01 05:05:03 -0700

    Added react-ga for google analytics

--- src/App/App.js
+++ src/App/App.js
@@ -2,8 +2,15 @@ import './App.css';
 import '../fontello/css/fontello.css';
 
 import React, {Component} from 'react';
+import ReactGa from 'react-ga';
 
 class App extends Component {
+  constructor() {
+    super();
+    ReactGA.initialize('UA-107328054-1');
+    ReactGA.pageview(window.location.pathname);
+  }
+  
   render() {
     return (
       <div className="App">
