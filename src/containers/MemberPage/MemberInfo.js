10a0f8cbb3e88ca443ed3f3cc74ee01d161a1f8e
Kaiser <kaiserkim167@gmail.com>
2017-10-12 14:44:56 -0700

    Added black background opacity when you hover over member image on member page

--- src/containers/MemberPage/MemberInfo.js
+++ src/containers/MemberPage/MemberInfo.js
@@ -8,12 +8,14 @@ class MemberInfo extends React.Component {
 
     return (
       <div>
-        <img
-          className="brother-image"
-          src={isSafari ? this.props.brother.professional_jpg : this.props.brother.professional_webp}
-          onClick={() => this.props.open(this.props.brother)}
-          alt="Active"
-        />
+        <div className="brother-image-container">
+          <img
+            className="brother-image"
+            src={isSafari ? this.props.brother.professional_jpg : this.props.brother.professional_webp}
+            onClick={() => this.props.open(this.props.brother)}
+            alt="Active"
+          />
+        </div>
         <h4> {this.props.brother.name.toUpperCase()} </h4>
         <p> {this.props.brother.position.toUpperCase()} </p>
         <p> {this.props.brother.className.toUpperCase()} </p>
