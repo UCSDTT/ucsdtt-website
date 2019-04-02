81c3f8fbb768de695b754f7d586246cf154bde91
Kaiser <kaiserkim167@gmail.com>
2019-04-02 02:29:12 -0700

    Added isMobile helper

--- src/shared/helpers.js
+++ src/shared/helpers.js
@@ -1,5 +1,15 @@
 var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
 
+function isMobile() {
+  return (navigator.userAgent.match(/Android/i) || 
+          navigator.userAgent.match(/webOS/i) || 
+          navigator.userAgent.match(/iPhone/i) || 
+          navigator.userAgent.match(/iPad/i) || 
+          navigator.userAgent.match(/iPod/i) || 
+          navigator.userAgent.match(/BlackBerry/i) || 
+          navigator.userAgent.match(/Windows Phone/i))
+};
+
 function sort(brothers) {
   brothers.sort(function(a, b) {
     if (a.name < b.name) return -1;
@@ -7,4 +17,4 @@ function sort(brothers) {
     return 0;
   });
 }
-export { isChrome, sort };
+export { isChrome, isMobile, sort };
