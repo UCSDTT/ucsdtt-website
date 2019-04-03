5b4579d2e6b3e9c0328780ff4fdf0fdcb4f4ae8b
Kaiser <kaiserkim167@gmail.com>
2019-04-03 16:27:54 -0700

    Cleaned up and flattened members section code

--- src/components/Members/BrothersList.js
+++ src/components/Members/BrothersList.js
@@ -6,88 +6,90 @@ import { FilteredLabel } from './FilteredLabel.js';
 import { MemberInfo } from './MemberInfo.js';
 
 class BrothersList extends React.Component {
-  renderFilteredList(option) {
-    let brothers = this.props.updatedBrothers;
-    let showList = false;
+  get brothersList() {
+    const { updatedBrothers } = this.props;
+    return (
+      <Row>
+        {updatedBrothers.map((brother, i) => (
+          <Col xs={6} sm={3} className="brother-info col-md-5th" key={i}>
+            <MemberInfo brother={brother} open={this.props.open} />
+          </Col>
+        ))}
+      </Row>
+    );
+  }
 
+  /* Displays labels if the selected filter is major or class */
+  get filteredBrothersList() {
+    const {
+      dropdownValue,
+      majorOptions,
+      classOptions,
+      specificValue,
+      updatedBrothers
+    } = this.props;
+    /* Sets label to major or class depending on which one is selected */
+    let options = dropdownValue === 'major' ? majorOptions : classOptions;
+
+    /* Sets the specific label based on which is selected */
+    if (specificValue) {
+      options = options.filter(option => {
+        return (option.value.toLowerCase() === specificValue.toLowerCase()) !== false;
+      });
+    }
+    return (
+      <div>
+        {options.map((option, i) => {
+          if (this.showFilteredList(option) === false) {
+            return null
+          }
+          return (
+            <Row key={i}>
+              <FilteredLabel option={option} specificValue={specificValue} />
+              {/* <FilteredList
+                option={option}
+                updatedBrothers={this.props.updatedBrothers}
+                dropdownValue={this.props.dropdownValue}
+                open={this.props.open}
+              /> */}
+              {updatedBrothers.map((brother, i) => {
+                if (brother[dropdownValue] !== option.value) {
+                  return null
+                }
+                return (
+                  <Col xs={6} sm={3} style={{ padding: 0 }}>
+                    <MemberInfo brother={brother} open={this.props.open} />
+                  </Col>
+                );
+              })}
+            </Row>
+          );
+        })}
+      </div>
+    );
+  }
+
+  /*
+    If there exists a brother in the specific major or class,
+    display the brothers of that specified major or class
+  */
+  showFilteredList(option) {
+    const { dropdownValue, updatedBrothers } = this.props;
     /* Checks if there exists a brother in the specified major or class */
-    brothers.forEach(brother => {
-      if (brother[this.props.dropdownValue] === option.value) {
-        showList = true;
+    updatedBrothers.forEach(brother => {
+      if (brother[dropdownValue] === option.value) {
+        return true
       }
     });
-
-    /*
-      If there exists a brother in the specific major or class,
-      display the brothers of that specified major or class
-    */
-    if (!showList) {
-      return false;
-    }
+    return false
   }
 
   render() {
-    let options;
-
-    /* Sets label to major or class depending on which one is selected */
-    if (this.props.dropdownValue === 'major') {
-      options = this.props.majorOptions;
-    } else {
-      options = this.props.classOptions;
+    const { dropdownValue } = this.props;
+    if (dropdownValue === 'major' || dropdownValue === 'class') {
+      return this.filteredBrothersList
     }
-
-    /* Sets the specific label based on which is selected */
-    if (this.props.specificValue) {
-      options = options.filter(option => {
-        return (option.value.toLowerCase() === this.props.specificValue.toLowerCase()) !== false;
-      });
-    }
-
-    /* Displays labels if the first dropdown value is major or class */
-    if (this.props.dropdownValue === 'major' || this.props.dropdownValue === 'class') {
-      return (
-        <div>
-          {options.map((option, i) => {
-            if (this.renderFilteredList(option) !== false) {
-              return (
-                <Row key={i}>
-                  <FilteredLabel option={option} specificValue={this.props.specificValue} />
-                  {/* <FilteredList
-                    option={option}
-                    updatedBrothers={this.props.updatedBrothers}
-                    dropdownValue={this.props.dropdownValue}
-                    open={this.props.open}
-                  /> */}
-                  {this.props.updatedBrothers.map((brother, i) => {
-                    if (brother[this.props.dropdownValue] === option.value) {
-                      return (
-                        <Col xs={6} sm={3} style={{ padding: 0 }}>
-                          <MemberInfo brother={brother} open={this.props.open} />
-                        </Col>
-                      );
-                    } else {
-                      return false;
-                    }
-                  })}
-                </Row>
-              );
-            } else {
-              return false;
-            }
-          })}
-        </div>
-      );
-    } else {
-      return (
-        <Row>
-          {this.props.updatedBrothers.map((brother, i) => (
-            <Col xs={6} sm={3} className="brother-info col-md-5th" key={i}>
-              <MemberInfo brother={brother} open={this.props.open} />
-            </Col>
-          ))}
-        </Row>
-      );
-    }
+    return this.brothersList
   }
 }
 
